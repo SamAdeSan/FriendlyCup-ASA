ca7cb40346ec691da18628247a3b68e784fe6e5c
Abelsaba6 <abelsabatermunoz@gmail.com>
2025-12-11 17:56:50 +0100

    Bases del proyecto

---
+++ src/Controller/PageController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class PageController extends AbstractController
+{
+    #[Route('/', name: 'inicio')]
+    public function index(): Response
+    {
+        return $this->render('page/index.html.twig', [
+            'controller_name' => 'PageController',
+        ]);
+    }
+    #[Route('/liga', name: 'liga')]
+    public function ligas(): Response
+    {
+        return $this->render('page/index.html.twig', [
+            'controller_name' => 'PageController',
+        ]);
+    }
+    #[Route('/creacionliga', name: 'creacionliga')]
+    public function creacionligas(): Response
+    {
+        return $this->render('page/index.html.twig', [
+            'controller_name' => 'PageController',
+        ]);
+    }
+    #[Route('/fantasy', name: 'fantasy')]
+    public function fantasy(): Response
+    {
+        return $this->render('page/index.html.twig', [
+            'controller_name' => 'PageController',
+        ]);
+    }
+    #[Route('/creacionfantasy', name: 'creacionfantasy')]
+    public function creacionfantasy(): Response
+    {
+        return $this->render('page/index.html.twig', [
+            'controller_name' => 'PageController',
+        ]);
+    }
+}
