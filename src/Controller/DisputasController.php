8a1dd044ad596b06e8be6952c7939cbe647f3694
Abelsaba06 <abelsabatermunoz@gmail.com>
2026-01-19 07:26:58 +0000

    Funcion crear disputas

--- src/Controller/DisputasController.php
+++ src/Controller/DisputasController.php
@@ -2,9 +2,17 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Disputas;
+use App\Repository\DisputasRepository;
+use App\Repository\EquiposRepository;
+use App\Repository\TorneoRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Doctrine\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class DisputasController extends AbstractController
 {
@@ -14,5 +22,21 @@ final class DisputasController extends AbstractController
         return $this->render('disputas/index.html.twig', [
             'controller_name' => 'DisputasController',
         ]);
+    }
+    #[Route('/disputas/crear', name: 'app_disputas')]
+    public function crearDisputas(Request $request, EntityManagerInterface $entityManager,EquiposRepository $equiposRepository,TorneoRepository $torneoRepository): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $disputa= new Disputas();
+        $disputa->setResultado('0-0');
+        $equipo1 = $equiposRepository->find($data['equipo1_id']);
+        $equipo2 = $equiposRepository->find($data['equipo2_id']);
+        $torneo  = $torneoRepository->find($data['torneo_id']);
+        $disputa->setEquipo1($equipo1);
+        $disputa->setEquipo2($equipo2);
+        $disputa->setTorneo($torneo);
+        $entityManager->persist($disputa);
+        $entityManager->flush();
+        return new JsonResponse($disputa->getId());
     }
 }
