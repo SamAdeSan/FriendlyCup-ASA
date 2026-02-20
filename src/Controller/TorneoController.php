4a9d80a8642d489071c08da007619dee193dae2f
Abelsaba06 <abelsabatermunoz@gmail.com>
2026-02-20 09:05:59 +0000

    Solucion problema de estadisticas

--- src/Controller/TorneoController.php
+++ src/Controller/TorneoController.php
@@ -6,8 +6,10 @@ use App\Entity\Torneo;
 use App\Entity\Equipos;
 use App\Form\EquipoFormType;
 use App\Form\TorneoFormType;
+use App\Entity\JugadorEvento;
 use App\Repository\EventoRepository;
 use App\Repository\TorneoRepository;
+use App\Repository\JugadorEventoRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
@@ -20,28 +22,29 @@ use Doctrine\Common\Collections\Criteria;
 final class TorneoController extends AbstractController
 {
     #[Route('/torneo/update/{id}', name: 'modificartorneo')]
-    public function updatetorneo(ManagerRegistry $doctrine, Request $request, $id): Response 
+    public function updatetorneo(ManagerRegistry $doctrine, Request $request, $id): Response
     {
-    $entityManager = $doctrine->getManager();
-    $repositorio = $doctrine->getRepository(Torneo::class);
-    $torneo = $repositorio->find($id);
-    if ($torneo) {
-        $formulario = $this->createForm(TorneoFormType::class, $torneo);
-        $formulario->handleRequest($request);
-        if ($formulario->isSubmitted() && $formulario->isValid()) {
-            $entityManager->flush(); 
-            return $this->redirectToRoute('torneo', ["id" => $torneo->getId()]);
+        $entityManager = $doctrine->getManager();
+        $repositorio = $doctrine->getRepository(Torneo::class);
+        $torneo = $repositorio->find($id);
+        if ($torneo) {
+            $formulario = $this->createForm(TorneoFormType::class, $torneo);
+            $formulario->handleRequest($request);
+            if ($formulario->isSubmitted() && $formulario->isValid()) {
+                $entityManager->flush();
+                return $this->redirectToRoute('torneo', ["id" => $torneo->getId()]);
+            }
+            return $this->render('page/editar-torneo.html.twig', [
+                'equipos' => $torneo->getEquipos(),
+                'formulario' => $formulario->createView(),
+                'torneo' => $torneo
+            ]);
         }
-        return $this->render('page/editar-torneo.html.twig', [
-            'equipos' => $torneo->getEquipos(),
-            'formulario' => $formulario->createView(),
-            'torneo' => $torneo
-        ]);
-    }
-    return $this->redirectToRoute('inicio');
+        return $this->redirectToRoute('inicio');
     }
     #[Route('/torneo/delete/{id}', name: 'torneoeliminado')]
-    public function delete(ManagerRegistry $doctrine, $id): Response{
+    public function delete(ManagerRegistry $doctrine, $id): Response
+    {
         $entityManager = $doctrine->getManager();
         $repositorio = $doctrine->getRepository(Torneo::class);
         $torneo = $repositorio->find($id);
@@ -58,7 +61,7 @@ final class TorneoController extends AbstractController
         if (!$this->getUser()) {
             return $this->redirectToRoute('app_login');
         }
-        $entityManager = $doctrine ->getManager();
+        $entityManager = $doctrine->getManager();
         $torneo = new Torneo();
         $formulario = $this->createForm(TorneoFormType::class, $torneo);
         $formulario->handleRequest($request);
@@ -68,7 +71,8 @@ final class TorneoController extends AbstractController
             $entityManager = $doctrine->getManager();
             $entityManager->persist($torneo);
             $entityManager->flush();
-            return $this->redirectToRoute('torneo', ["id" => $torneo->getId()]);        }
+            return $this->redirectToRoute('torneo', ["id" => $torneo->getId()]);
+        }
         return $this->render('page/crear-torneo.html.twig', [
             'controller_name' => 'PageController',
             'formulario' => $formulario->createView()
@@ -77,7 +81,7 @@ final class TorneoController extends AbstractController
     #[Route('/torneos', name: 'lista_torneos')]
     public function index(TorneoRepository $repo, Request $request): Response
     {
-        $termino = $request->query->get('q'); 
+        $termino = $request->query->get('q');
 
         if ($termino) {
             $torneos = $repo->buscarPorNombre($termino);
@@ -91,8 +95,9 @@ final class TorneoController extends AbstractController
         ]);
     }
     #[Route('/torneo/{id}/crearequipo', name: 'crearequipo')]
-    public function crearEquipo(int $id,ManagerRegistry $doctrine,Request $request): Response {
-        
+    public function crearEquipo(int $id, ManagerRegistry $doctrine, Request $request): Response
+    {
+
         $entityManager = $doctrine->getManager();
         $torneoRepo = $doctrine->getRepository(Torneo::class);
         $torneo = $torneoRepo->find($id);
@@ -117,67 +122,62 @@ final class TorneoController extends AbstractController
         ]);
     }
     #[Route('/torneo/{id}/seguir', name: 'torneo_seguir', methods: ['POST'])]
-    public function seguir(Torneo $torneo,EntityManagerInterface $em): JsonResponse
+    public function seguir(Torneo $torneo, EntityManagerInterface $em): JsonResponse
     {
         $user = $this->getUser();
         if ($user->getSeguidos()->contains($torneo)) {
             $user->removeSeguido($torneo);
             $accion = 'desseguir';
-        }else{
+        } else {
             $user->addSeguido($torneo);
             $accion = 'seguir';
         }
         $em->flush();
         return $this->json([
-        'accion' => $accion,
-        'totalSeguidores' => $torneo->getSeguidores()->count()
+            'accion' => $accion,
+            'totalSeguidores' => $torneo->getSeguidores()->count()
         ]);
     }
     #[Route('/torneo/{id}/resumen', name: 'torneo_resumen')]
     public function resumen(Torneo $torneo): Response
     {
-        $admin=$torneo->getOrganizador();        
+        $admin = $torneo->getOrganizador();
         $disputas = $torneo->getDisputas();
 
         return $this->render('page/torneo/resumen.html.twig', [
             'torneo' => $torneo,
-            'admin'  => $admin,
-            'disputas'  => $disputas->toArray()
+            'admin' => $admin,
+            'disputas' => $disputas->toArray()
         ]);
     }
     #[Route('/torneo/{id}/clasificacion', name: 'torneo_clasificacion')]
     public function clasificacion(Torneo $torneo): Response
     {
         // Creamos un criterio de ordenación
-    $criteria = Criteria::create()
-        ->orderBy(['puntos' => Criteria::DESC]); // DESC = de mayor a menor
+        $criteria = Criteria::create()
+            ->orderBy(['puntos' => Criteria::DESC]); // DESC = de mayor a menor
         $equiposOrdenados = $torneo->getEquipos()->matching($criteria);
         return $this->render('page/torneo/clasificacion.html.twig', [
             'equipos' => $equiposOrdenados
         ]);
     }
     #[Route('/torneo/{id}/estadisticas', name: 'torneo_estadisticas')]
-    public function estadisticas(Torneo $torneo,EventoRepository $estadisticas): Response
+    public function estadisticas(Torneo $torneo, EventoRepository $eventoRepo): Response
     {
-        $jugadores =[];
-        foreach ($torneo->getEquipos() as $equipo) {
-            foreach ($equipo->getJugadores() as $jugador) {
-                $jugadores[$jugador->getId()] = $jugador;
-            }
-        }
-        $estadisticas = $estadisticas->findBy(['torneo' => $torneo]);
+        // Obtenemos todos los eventos (tipos de estadística) de este torneo
+        $eventos = $eventoRepo->findBy(['torneo' => $torneo]);
+
         return $this->render('page/torneo/estadisticas.html.twig', [
-            'jugadores' => $jugadores,
             'torneo' => $torneo,
-            'estadisticas' => $estadisticas
+            'eventos' => $eventos
         ]);
     }
     #[Route('/torneo/{id}', name: 'torneo')]
-    public function ligas(int $id,ManagerRegistry $doctrine): Response
+    public function ligas(int $id, ManagerRegistry $doctrine): Response
     {
         $repositorio = $doctrine->getRepository(Torneo::class);
-        $torneo=$repositorio->find($id);
-        $admin=$torneo->getOrganizador();
+        $torneo = $repositorio->find($id);
+        $admin = $torneo->getOrganizador();
         $user = $this->getUser();
         $yaSigue = false;
         $disputas = $torneo->getDisputas();
@@ -186,9 +186,9 @@ final class TorneoController extends AbstractController
         }
         return $this->render('page/torneos.html.twig', [
             'torneo' => $torneo,
-            'admin'  => $admin,
+            'admin' => $admin,
             'yasigue' => $yaSigue,
-            'disputas'=> $disputas
+            'disputas' => $disputas
         ]);
     }
 }
