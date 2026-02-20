9d75064a36427e0162a9fd3414cc2a56c8fcb2a3
SamAdeSan <samadesan@alu.edu.gva.es>
2026-02-20 08:22:47 +0100

    Editado el CSS de la página al completo de manera profesional y solucionado el CRUD de la imagen de perfil

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -3,18 +3,21 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class ProfileController extends AbstractController
 {
-    #[Route('/profile/{username}', name: 'user_profile')]
-    public function index(string $username): Response
+    /**
+     * Actualizar el avatar del usuario logueado
+     */
+    #[Route('/profile/actualizar-avatar', name: 'user_update_avatar', methods: ['POST'])]
+    public function updateAvatar(Request $request, EntityManagerInterface $entityManager): Response
     {
         /** @var User $user */
         $user = $this->getUser();
@@ -23,30 +26,15 @@ class ProfileController extends AbstractController
             return $this->redirectToRoute('app_login');
         }
 
-        return $this->render('profile/index.html.twig', [
-            'user' => $user,
-            'torneos' => $user->getTorneos(), // Ajusta según tu relación
-        ]);
-    }
-
-    #[Route('/profile/actualizar-avatar', name: 'user_update_avatar', methods: ['POST'])]
-    public function updateAvatar(Request $request, SluggerInterface $slugger, EntityManagerInterface $entityManager): Response
-    {
-        /** @var User $user */
-        $user = $this->getUser();
         $fotoFile = $request->files->get('avatar');
 
         if ($fotoFile) {
             $newFilename = uniqid().'.'.$fotoFile->guessExtension();
 
             try {
-                // USAR EL PARÁMETRO DE SERVICES.YAML
-                $fotoFile->move(
-                    $this->getParameter('avatars_directory'),
-                    $newFilename
-                );
+                $fotoFile->move($this->getParameter('avatars_directory'), $newFilename);
 
-                // Borrar antigua si existe
+                // Borrar foto antigua si existe para ahorrar espacio
                 if ($user->getFoto()) {
                     $oldPath = $this->getParameter('avatars_directory').'/'.$user->getFoto();
                     if (file_exists($oldPath)) {
@@ -55,17 +43,21 @@ class ProfileController extends AbstractController
                 }
 
                 $user->setFoto($newFilename);
+                $entityManager->persist($user);
                 $entityManager->flush();
 
-                $this->addFlash('success', 'Imagen actualizada.');
+                $this->addFlash('success', '¡Imagen actualizada!');
             } catch (FileException $e) {
-                // Manejar error
+                $this->addFlash('error', 'Error al subir la imagen.');
             }
         }
 
-        return $this->redirectToRoute('user_profile', ['username' => $user->getUserIdentifier()]);
+        return $this->redirectToRoute('user_profile', ['username' => $user->getName()]);
     }
 
+    /**
+     * Borrar el avatar actual
+     */
     #[Route('/profile/borrar-avatar', name: 'user_delete_avatar', methods: ['POST'])]
     public function deleteAvatar(EntityManagerInterface $entityManager): Response
     {
@@ -73,18 +65,34 @@ class ProfileController extends AbstractController
         $user = $this->getUser();
 
         if ($user && $user->getFoto()) {
-            $filePath = $this->getParameter('kernel.project_dir') . '/public/uploads/avatars/' . $user->getFoto();
-
+            $filePath = $this->getParameter('avatars_directory') . '/' . $user->getFoto();
             if (file_exists($filePath)) {
                 unlink($filePath);
             }
 
             $user->setFoto(null);
             $entityManager->flush();
-
             $this->addFlash('success', 'Foto eliminada.');
         }
 
-        return $this->redirectToRoute('user_profile', ['username' => $user->getUserIdentifier()]);
+        return $this->redirectToRoute('user_profile', ['username' => $user->getName()]);
+    }
+
+    /**
+     * Ver perfil de cualquier usuario (esta ruta va al final para evitar conflictos)
+     */
+    #[Route('/profile/{username}', name: 'user_profile', requirements: ['username' => '^(?!actualizar-avatar|borrar-avatar).+'])]
+    public function index(string $username, UserRepository $userRepo): Response
+    {
+        $userRequested = $userRepo->findOneBy(['name' => $username]);
+
+        if (!$userRequested) {
+            throw $this->createNotFoundException('El usuario no existe.');
+        }
+
+        return $this->render('profile/index.html.twig', [
+            'user' => $userRequested,
+            'torneos' => $userRequested->getTorneos(),
+        ]);
     }
 }
